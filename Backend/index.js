236e668e3702a6385544948c879a38cae3ae3c80
Pratik KS <sahoopratik5@gmail.com>
2024-08-15 10:45:15 +0530

    Fixed: Little bugs

--- Backend/index.js
+++ Backend/index.js
@@ -18,22 +18,24 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// const corsOptions = {
-// 	origin: "http://localhost:5173",
-// 	credentials: true,
-// };
+const corsOptions = {
+	origin: "http://localhost:5173",
+	credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, "../Frontend/dist"))); 
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
-});
-// app.use(cors(corsOptions));
 
 //API
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
+
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
 	connectDB();
